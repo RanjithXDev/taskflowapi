a772ce1fcb2c4010bb222503d43eba0435bef6a1
ranjith.mv <ranjith.mv@grootan.com>
2026-03-08 17:47:39 +0530

    migration(db) - mongo local to atlas migrated

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -32,17 +32,70 @@ const router = Router();
  *         description: Invalid credentials
  */
 router.post("/signup",avatarUpload.single("avatar"), authLimiter, signupValidator, validateRequest, signup);
-
+/**
+ * @swagger
+ * /api/auth/login:
+ *   post:
+ *     summary: User Login
+ *     tags: [Auth]
+ */
 router.post("/login", authLimiter, loginValidator, validateRequest, login);
+/**
+ * @swagger
+ * /api/auth/logout:
+ *   post:
+ *     summary: Logout the current user and clear the cookies
+ *     tags: [Auth]
+ */
 router.post("/logout", logout);
-
+/**
+ * @swagger
+ * /api/auth/refresh:
+ *   post:
+ *     summary: Refresh access token
+ *     tags: [Auth]
+ */
 router.post("/refresh", refresh);
-
+/**
+ * @swagger
+ * /api/auth/forgot-password:
+ *   post:
+ *     summary: Send password reset email
+ *     tags: [Auth]
+ */
 router.post("/forgot-password", authLimiter, forgotPasswordValidator, validateRequest, forgotPassword);
+/**
+ * @swagger
+ * /api/auth/me/avatar:
+ *   put:
+ *     summary: Upload user avatar
+ *     tags: [Auth]
+ */
 router.put("/me/avatar", isAuth,  avatarUpload.single("avatar"), uploadAvatar);
-router.post("/reset-password/:token", resetPasswordValidator, validateRequest, resetPassword);
-router.get("/verify-email/:token", verifyEmail);
 
+/**
+ * @swagger
+ * /api/auth/reset-password/{token}:
+ *   post:
+ *     summary: Reset password using token
+ *     tags: [Auth]
+ */
+router.post("/reset-password/:token", resetPasswordValidator, validateRequest, resetPassword);
+/**
+ * @swagger
+ * /api/auth/verify-email/{token}:
+ *   get:
+ *     summary: Verify user email
+ *     tags: [Auth]
+ */
+router.get("/verify-email/:token", verifyEmail);
+/**
+ * @swagger
+ * /api/auth/me:
+ *   get:
+ *     summary: Get logged in user profile
+ *     tags: [Auth]
+ */
 router.get("/me", isAuth, me);
 
 export default router;
